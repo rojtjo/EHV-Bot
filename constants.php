506ab2317bbbfc105413fc9e50b98a8302e98813
kevinnewesil <newesil.kevin@gmail.com>
2011-12-22 13:48:15 +0100

    commit test

--- constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 
- */
-define('FULL_PATH', '/Users/iRoj/Sites/school/arduino/');
-define('CONFIG_PATH', FULL_PATH . 'config/');
-define('INC_PATH', FULL_PATH . 'inc/');
-define('VIEW_PATH', FULL_PATH . 'views/');
-define('BASE_URL', 'http://localhost/school/arduino/');
